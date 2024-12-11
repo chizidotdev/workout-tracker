db1c33a3c313db245aa7f9f6be4cf42246444089
Chizi Victor <chiziwokoma@gmail.com>
2024-12-11 10:28:53 +0100

    feat: use localStorage for persisted cache

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,28 +3,30 @@
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.client
  */
+import { RemixBrowser } from "@remix-run/react";
 import { StrictMode, startTransition } from "react";
 
-import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
-
-// import { CacheAdapter, configureGlobalCache } from "remix-client-cache";
+import { CacheAdapter, configureGlobalCache } from "remix-client-cache";
 
 // import { queryClient } from "./root";
 
-// class ReactQueryAdapter implements CacheAdapter {
-//   async getItem(key: string) {
-//     return queryClient.getQueryData([key]);
-//   }
-//   async setItem(key: string, value: string) {
-//     return queryClient.setQueryData([key], value);
-//   }
-//   async removeItem(key: string) {
-//     return queryClient.removeQueries({ queryKey: [key] });
-//   }
-// }
+class LocalStorageAdapter implements CacheAdapter {
+  async getItem(key: string) {
+    const value = localStorage.getItem(key);
+    if (!value) return;
 
-// configureGlobalCache(() => new ReactQueryAdapter());
+    return JSON.parse(value);
+  }
+  async setItem(key: string, value: string) {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+  async removeItem(key: string) {
+    return localStorage.removeItem(key);
+  }
+}
+
+configureGlobalCache(() => new LocalStorageAdapter());
 
 startTransition(() => {
   hydrateRoot(
